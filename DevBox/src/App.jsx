8e3e551d79ad38c2b096f4528440281aac8e114c
daniel.akinmaiyejolu <daniel.akinmaiyejolu@avanade.com>
2024-02-16 12:55:13 +0000

    first commit

---
+++ DevBox/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/NavBar";
+
+const App = () => {
+  return (
+    <div>
+      <Background/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
